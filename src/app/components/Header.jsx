ef26012043b58ae9012ce2dc746978a353aabd2f
Younes Kazemi <youneskazemi9798@gmail.com>
2025-07-31 13:11:12 +0330

    feat: header and footer

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Menu } from "lucide-react"
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="flex sticky top-1 z-50 justify-between items-center p-4 lg:p-6 bg-white text-primary rounded-[8px] lg:rounded-[24px] mt-[13px] mx-[14px] lg:mx-[71px] lg:mt-[45px]">
+        <div className="flex items-center gap-2 lg:gap-5">
+            <img src="/logo.png" alt="logo" className="w-[45px] h-[24px] lg:w-[60px] lg:h-[32px]"/>
+            <h1 className="text-[14px] lg:text-[30px]">IMD Token</h1>
+        </div>
+        <nav className="hidden lg:flex">
+            <ul className="flex items-center gap-2 text-[18px]">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/tokenomics">Tokenomics</Link></li>
+                <li><Link href="/whitepaper">White paper</Link></li>
+              
+            </ul>
+        </nav>
+        <nav className="lg:hidden">
+            <button>
+                <Menu color="#FF4135" size={30}/>
+                
+            </button>
+        </nav>
+        <div className="hidden lg:flex">
+            <button>Login</button>
+        </div>
+    </header>
   )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import { Footer } from "./components/Footer";
 
+// Keep Geist fonts for fallback
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-iransans`}
       >
         <Header />
         {children}
