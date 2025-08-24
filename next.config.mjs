441e0f4a19426ef50d60d2ec1e3f3db2b727450d
Younes Kazemi <youneskazemi9798@gmail.com>
2025-08-24 08:30:07 +0330

    fix: some warnings on duplicate package-lock file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.js");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingRoot: __dirname,
+};
 
 export default withNextIntl(nextConfig);
