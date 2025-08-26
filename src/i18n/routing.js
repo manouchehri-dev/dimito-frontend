d76d5276a69d2af1d0a1fb856a48027953542585
Younes Kazemi <youneskazemi9798@gmail.com>
2025-08-26 14:51:37 +0330

    fix: remove en for now

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -2,8 +2,8 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "fa"],
+  locales: ["fa"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "fa",
 });
